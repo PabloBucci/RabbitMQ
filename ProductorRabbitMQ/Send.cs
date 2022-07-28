82a0f2bed404bcb2b2e583cb41afe973bda78a72
PabloBucci <pablo.jb84@gmail.com>
2022-07-28 18:16:44 -0300

    Se sube Productor y Consumidor
    Proyectos básicos para el intercambio de mensajes mediante RabbitMQ

---
+++ ProductorRabbitMQ/Send.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace ProductorRabbitMQ
+{
+    class send
+    {
+        static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "Q2",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine($"[x] Sent {message}");
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
